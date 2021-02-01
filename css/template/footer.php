96884a21dee98cf192b07976a026567c11686ae8
kd1970 <giovanikade@hotmail.com>
2021-02-01 20:57:10 -0300

    v001

---
+++ css/template/footer.php
@@ -0,0 +1,15 @@
+        <!-- Page footer -->
+        <footer class="row tm-page-footer">
+            <p class="col-12 tm-copyright-text mb-0">
+                Copyright &copy; 2021 Todos os direitos Reservados . Designed by
+                <a href="http://www.rexxdevs.com" rel="nofollow" class="tm-copyright-link">RexxDevs.com</a>
+            </p>
+        </footer>
+    </div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/parallax.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
