7ab4905d5303789e2740a1e75139eef494196c03
kd1970 <giovanikade@hotmail.com>
2021-02-01 23:23:18 -0300

    v001

--- index.php
+++ index.php
@@ -2,9 +2,73 @@
     include 'template/header.php'
 ?>
 
-<?php 
-    include 'template/content.php'
-?>
+<div class="row tm-welcome-row">
+            <div class="col-12 tm-page-cols-container">
+                <div class="tm-page-col-left tm-welcome-box tm-bg-primary">
+                    <p class="tm-welcome-text">
+                        <em>"Saiba tudo sobre as melhores festas e os mais badalados eventos pelo Brasil e pelo mundo"</em>
+                    </p>
+                </div>
+                <div class="tm-page-col-right">
+                    <div class="tm-welcome-parallax" data-parallax="scroll" data-image-src="img/saloon.jpg"></div>
+                </div>
+            </div>
+        </div>
+
+        <section class="row tm-pt-4 tm-pb-6">
+            <div class="col-12 tm-page-cols-container">
+                <div class="tm-page-col-right">
+                    <h2 class="tm-text-secondary tm-mb-5">
+                        Nas pistas
+                    </h2>
+                    <p class="tm-mb-6">
+                        Buscar eventos e apresenta-los a <strong>voçê cliente</strong> para garantir sua total <strong>diversão</strong>. Estamos sempre antenados com o que há de melhor em festas e eventos.
+                        Voçê pode sempre contar conosco para sua total diversão.
+
+                    </p>
+                    <p class="mb-0">
+                        Por aqui voçê sempre estará por dentro de tudo que acontece no Brasil e no exterior sobre festas e viagens.Siga nossas redes socias e divirta-se.
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <div class="tm-page-col-right">
+            <div class="row tm-pt-7 tm-pb-6">
+                <div class="col-md-6 tm-home-section-2-left">
+                    <div class="img-fluid tm-mb-4 tm-small-parallax" data-parallax="scroll" data-image-src="img/image-1.jpg"></div>
+                    <div>
+                        <h3 class="tm-text-secondary tm-mb-4">
+                            Lives, Haves, Encontros?
+                        </h3>
+                        <p class="tm-mb-5">
+                            As melhores festas espalhads pelo país e aqui seu roteiro é completo.
+                        </p>
+                        <ul class="tm-list-plus">
+                            <li>Acesse aqui o calendário de Eventos</li>
+                            
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-6 tm-home-section-2-right">
+                    <div class="img-fluid tm-mb-4 tm-small-parallax" data-parallax="scroll" data-image-src="img/image-2.jpg"></div>
+                    <div>
+                        <h3 class="tm-text-secondary tm-mb-4">
+                            As melhores músicas e Dj's
+                        </h3>
+                        <p class="tm-section-2-text">
+                            Músicas <strong>os melhores DJ's </strong>  <strong>as melhores músicas </strong> e claro tudo pensando em voçê.
+                            fique atento as nossas dicas diárias de shows e promoções.
+                        </p>
+                        <ul class="tm-list-plus">
+                            <li>Ouça aqui grandes sons tocados só pelos melhores DJ's. </li>
+                            
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- row -->
 
 <?php 
     include 'template/footer.php'
